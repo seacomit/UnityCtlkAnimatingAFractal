36fc90d589e4333bb5323baf0bf7b897611ff03c
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-27 14:41:45 -0400

    Complete fractal for 1 dimension

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -15,16 +15,23 @@ public class Fractal : MonoBehaviour
             return;
         }
 
+        Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
+        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
+        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
+
+        childA.transform.SetParent(transform, false);
+        childB.transform.SetParent(transform, false);
+        childC.transform.SetParent(transform, false);
+    }
+
+    Fractal CreateChild (Vector3 direction, Quaternion rotation)
+    {
         Fractal child = Instantiate(this);
         child.depth = depth - 1;
-        child.transform.SetParent(transform, false);
-        child.transform.localPosition = 0.75f * Vector3.right;
+        child.transform.localPosition = 0.75f * direction;
+        child.transform.localRotation = rotation;
         child.transform.localScale = 0.5f * Vector3.one;
 
-        child = Instantiate(this);
-        child.depth = depth - 1;
-        child.transform.SetParent(transform, false);
-        child.transform.localPosition = 0.75f * Vector3.up;
-        child.transform.localScale = 0.5f * Vector3.one;
+        return child;
     }
 }
