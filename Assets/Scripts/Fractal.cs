54870e631024a7402cca62fe2204509700402053
versatus <86527888+seacomit@users.noreply.github.com>
2023-06-28 09:23:51 -0400

    Initial code for flattening the hierarchy

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -4,43 +4,85 @@ using UnityEngine;
 
 public class Fractal : MonoBehaviour
 {
+    struct FractalPart
+    {
+        public Vector3 direction;
+        public Quaternion rotation;
+        public Transform transform;
+    }
+
     [SerializeField, Range(1, 8)]
     int depth = 4;
 
-    private void Start()
+    [SerializeField]
+    Mesh mesh;
+
+    [SerializeField]
+    Material material;
+
+    FractalPart[][] parts;
+
+    static Vector3[] directions = {
+        Vector3.up, Vector3.right, Vector3.left, Vector3.forward, Vector3.back
+    };
+
+    static Quaternion[] rotations = {
+        Quaternion.identity,
+        Quaternion.Euler(0f, 0f, -90f), Quaternion.Euler(0f, 0f, 90f),
+        Quaternion.Euler(90f, 0f, 0f), Quaternion.Euler(-90f, 0f, 0f)
+    };
+
+    FractalPart CreatePart(int levelIndex, int childIndex, float scale) 
     {
-        name = "Fractal" + depth;
-        if (depth <= 1)
+        var go = new GameObject("Fractal Part" + levelIndex + " C" + childIndex);
+        go.transform.SetParent(transform, false);
+        go.AddComponent<MeshFilter>().mesh = mesh;
+        go.AddComponent<MeshRenderer>().material = material;
+        go.transform.localScale = scale * Vector3.one;
+
+        return new FractalPart { 
+            direction = directions[childIndex],
+            rotation = rotations[childIndex],
+            transform = go.transform
+        };
+    }
+
+    void Awake()
+    {
+        parts = new FractalPart[depth][];
+        for (int i = 0, length = 1; i < parts.Length; i++, length *= 5)
         {
-            return;
+            parts[i] = new FractalPart[length];
         }
 
-        Fractal childA = CreateChild(Vector3.up, Quaternion.identity);
-        Fractal childB = CreateChild(Vector3.right, Quaternion.Euler(0f, 0f, -90f));
-        Fractal childC = CreateChild(Vector3.left, Quaternion.Euler(0f, 0f, 90f));
-        Fractal childD = CreateChild(Vector3.forward, Quaternion.Euler(90f, 0f, 0));
-        Fractal childE = CreateChild(Vector3.back, Quaternion.Euler(-90f, 0f, 0));
-
-        childA.transform.SetParent(transform, false);
-        childB.transform.SetParent(transform, false);
-        childC.transform.SetParent(transform, false);
-        childD.transform.SetParent(transform, false);
-        childE.transform.SetParent(transform, false);
+        float scale = 1f;
+        parts[0][0] = CreatePart(0, 0, scale);
+        for (int li = 1; li < parts.Length; li++)
+        {
+            scale *= 0.5f;
+            FractalPart[] levelParts = parts[li];
+            for (int fpi = 0; fpi < levelParts.Length; fpi += 5)
+            {
+                for (int ci = 0; ci < 5; ci++)
+                {
+                    levelParts[fpi + ci] = CreatePart(li, ci, scale);
+                }
+            }
+        }
     }
 
-    private void Update()
+    void Update()
     {
-        transform.Rotate(0f, 22.5f * Time.deltaTime, 0f);
-    }
-
-    Fractal CreateChild (Vector3 direction, Quaternion rotation)
-    {
-        Fractal child = Instantiate(this);
-        child.depth = depth - 1;
-        child.transform.localPosition = 0.75f * direction;
-        child.transform.localRotation = rotation;
-        child.transform.localScale = 0.5f * Vector3.one;
-
-        return child;
+        for (int li = 1; li < parts.Length; li++)
+        {
+            FractalPart[] parentParts = parts[li - 1];
+            FractalPart[] levelParts = parts[li];
+            for (int fpi = 0; fpi < levelParts.Length; fpi++)
+            {
+                Transform parentTransform = parentParts[fpi / 5].transform;
+                FractalPart part = levelParts[fpi];
+                part.transform.localPosition = parentTransform.localPosition + 1.5f * part.transform.localScale.x * part.direction;
+            }
+        }
     }
 }
